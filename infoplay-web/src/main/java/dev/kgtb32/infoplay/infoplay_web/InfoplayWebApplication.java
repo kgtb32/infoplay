67849946c0695dc26f1ba07314e8a774e08c5fc8
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-10-14 09:42:22 +0200

    Init project

---
+++ infoplay-web/src/main/java/dev/kgtb32/infoplay/infoplay_web/InfoplayWebApplication.java
@@ -0,0 +1,13 @@
+package dev.kgtb32.infoplay.infoplay_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoplayWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoplayWebApplication.class, args);
+	}
+
+}
---
+++ infoplay-web/src/test/java/dev/kgtb32/infoplay/infoplay_web/InfoplayWebApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kgtb32.infoplay.infoplay_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoplayWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
