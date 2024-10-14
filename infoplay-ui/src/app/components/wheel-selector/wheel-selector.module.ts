850ff33e32d8f1db830d429e7d3da237831a6831
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-10-14 10:37:57 +0200

    Add wheel-selector component and module

---
+++ infoplay-ui/src/app/components/wheel-selector/wheel-selector.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WheelSelectorComponent } from './wheel-selector.component';
+
+
+
+@NgModule({
+  declarations: [
+    WheelSelectorComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    WheelSelectorComponent
+  ]
+})
+export class WheelSelectorModule { }
