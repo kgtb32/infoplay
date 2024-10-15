771eb0c4c30fd95c393620b2536e7d40f6948ee1
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-10-15 15:34:12 +0200

    Add joypad hability to wheel component

--- infoplay-ui/src/app/components/wheel-selector/wheel-selector.component.ts
+++ infoplay-ui/src/app/components/wheel-selector/wheel-selector.component.ts
@@ -1,15 +1,46 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
 import { WheelSelectorItem } from '../../models/components/wheel-selector-item';
 import { slice } from 'lodash'
 import { moveUp } from '../../services/wheel-selector/wheel-selector-up';
+import { JoypadService } from '../../services/joypad.service';
+import { ButtonPressedDetails } from '../../models/core/joypad/joypad-connect-event';
+import { AudioService } from '../../services/audio.service';
 
 @Component({
   selector: 'app-wheel-selector',
   templateUrl: './wheel-selector.component.html',
-  styleUrl: './wheel-selector.component.scss'
+  styleUrl: './wheel-selector.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WheelSelectorComponent {
+export class WheelSelectorComponent implements AfterViewInit {
   private static readonly MAX_ITEMS = 16
+  private static readonly THREESHOLD = 150
+
+  private lastMovement: number = new Date().getTime()
+
+  constructor(private readonly joypadService: JoypadService, private readonly cd: ChangeDetectorRef, private readonly audioService: AudioService) {
+  }
+
+  ngAfterViewInit(): void {
+    this.joypadService.axisMoveEvent.subscribe({
+      next: button => this.axisMoved.bind(this)(button),
+    })
+    this.joypadService.buttonPressEvent.subscribe({
+      next: () => this.audioService.valid()
+    })
+  }
+
+  private axisMoved(button: ButtonPressedDetails) {
+    if (new Date().getTime() - this.lastMovement > WheelSelectorComponent.THREESHOLD) {
+      this.lastMovement = new Date().getTime()
+      if (button.directionOfMovement == 'left') {
+        this.moveDown()
+      }
+      else if (button.directionOfMovement == 'right') {
+        this.moveUp()
+      }
+    }
+  }
 
   @Input()
   set items(items: WheelSelectorItem[]) {
@@ -35,11 +66,15 @@ export class WheelSelectorComponent {
     this.selectedIndex++
     this.boundsSelectedIndex('UP')
     this.visibleItems = moveUp(WheelSelectorComponent.MAX_ITEMS, this.selectedIndex, this._items)
+    this.cd.detectChanges()
+    this.audioService.select()
   }
 
   moveDown() {
     this.selectedIndex--
     this.boundsSelectedIndex('DOWN')
     this.visibleItems = moveUp(WheelSelectorComponent.MAX_ITEMS, this.selectedIndex, this._items)
+    this.cd.detectChanges()
+    this.audioService.select()
   }
 }
