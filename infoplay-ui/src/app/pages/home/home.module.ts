b006b435cd2a99766b21b1688b32ba47ee018c2c
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-10-18 08:56:16 +0200

    Removed Home list component

--- infoplay-ui/src/app/pages/home/home.module.ts
+++ infoplay-ui/src/app/pages/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { WheelSelectorModule } from "../../components/wheel-selector/wheel-selector.module";
+import { HomeListModule } from '../../components/home-list/home-list.module';
 
 
 
@@ -11,7 +12,8 @@ import { WheelSelectorModule } from "../../components/wheel-selector/wheel-selec
   ],
   imports: [
     CommonModule,
-    WheelSelectorModule
+    WheelSelectorModule,
+    HomeListModule
   ]
 })
 export class HomeModule { }
