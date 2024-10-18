3483931d5f166c34f46bdb30b9e07dc49ed67210
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-10-18 15:25:41 +0200

    Add Upload Folder configuration

---
+++ infoplay-web/src/main/java/dev/kgtb32/infoplay/infoplay_web/config/UploadFolderConfiguration.java
@@ -0,0 +1,10 @@
+package dev.kgtb32.infoplay.infoplay_web.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("app.upload-folder")
+public record UploadFolderConfiguration(
+    String imageDestinationFolder,
+    String gameDestinationFolder
+) {
+}
