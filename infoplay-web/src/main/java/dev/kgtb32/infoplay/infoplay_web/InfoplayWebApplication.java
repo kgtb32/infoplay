ded29c46860352dbbc61e86368801d397420ac0b
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-10-18 15:27:48 +0200

    Add create game API

--- infoplay-web/src/main/java/dev/kgtb32/infoplay/infoplay_web/InfoplayWebApplication.java
+++ infoplay-web/src/main/java/dev/kgtb32/infoplay/infoplay_web/InfoplayWebApplication.java
@@ -2,7 +2,9 @@ package dev.kgtb32.infoplay.infoplay_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class InfoplayWebApplication {
 
---
+++ infoplay-web/src/main/java/dev/kgtb32/infoplay/infoplay_web/services/GameService.java
@@ -0,0 +1,50 @@
+package dev.kgtb32.infoplay.infoplay_web.services;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import dev.kgtb32.infoplay.infoplay_web.config.UploadFolderConfiguration;
+import dev.kgtb32.infoplay.infoplay_web.mappers.GameDtoEntityMapper;
+import dev.kgtb32.infoplay.infoplay_web.models.dto.GameCreateDto;
+import dev.kgtb32.infoplay.infoplay_web.models.dto.GameResponseDto;
+import dev.kgtb32.infoplay.infoplay_web.repository.GameRepository;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class GameService {
+
+    private final UploadFolderConfiguration uploadFolderConfiguration;
+    private final GameRepository gameRepository;
+    private final GameDtoEntityMapper gameMapper;
+
+    private File saveFile(MultipartFile file, String type) throws IOException{
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+        String destFolder = type.equals("image") ? uploadFolderConfiguration.imageDestinationFolder() : uploadFolderConfiguration.gameDestinationFolder();
+        File destinationFile = new File(destFolder+UUID.randomUUID()+"."+extension);
+        file.transferTo(destinationFile);
+        return destinationFile;
+    }
+
+    public Optional<GameResponseDto> createGame(GameCreateDto gameCreateDto, MultipartFile image, MultipartFile game){
+        File imageFile;
+        File gameFile;
+        try{
+           gameFile =  saveFile(game, "game");
+           imageFile = saveFile(image, "image");
+           return Optional.of(
+            gameMapper.gameToResponseDto(
+                gameRepository.save(gameMapper.dtoToGame(gameCreateDto, gameFile, imageFile)))
+            );
+        }
+        catch(IOException e){
+            return Optional.empty();
+        }
+    }
+}
