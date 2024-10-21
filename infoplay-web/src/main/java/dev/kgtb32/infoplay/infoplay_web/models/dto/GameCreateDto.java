7cadca2baa741d58972e09c0f987f855201b0b63
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-10-21 09:00:40 +0200

    Fixed backend related issues

--- infoplay-web/src/main/java/dev/kgtb32/infoplay/infoplay_web/models/dto/GameCreateDto.java
+++ infoplay-web/src/main/java/dev/kgtb32/infoplay/infoplay_web/models/dto/GameCreateDto.java
@@ -1,12 +1,12 @@
 package dev.kgtb32.infoplay.infoplay_web.models.dto;
 
-import io.micrometer.common.lang.Nullable;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 
 public record GameCreateDto(
     @NotBlank
     String name,
-    @Nullable
+    @Valid
     GameDescriptionDto description,
     boolean favorite
 ){
