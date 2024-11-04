77d2fac03c232e83d8db446d9554d1bd1e7a1c4d
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-11-04 11:58:52 +0100

    Add direct mode change event in InlineLIst component

--- infoplay-ui/src/app/services/menu/menu-state.service.ts
+++ infoplay-ui/src/app/services/menu/menu-state.service.ts
@@ -2,6 +2,7 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { filter } from 'rxjs';
 import { InlineListMetadata } from '../../models/components/inline-list-metadata';
 import { MenuOpenState } from '../../models/core/state/menu-open-state';
+import { WheelSelectorItem } from '../../models/components/wheel-selector-item';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,8 @@ export class MenuStateService {
 
   public readonly menuChanged: EventEmitter<InlineListMetadata> = new EventEmitter()
   public readonly menuOpen: EventEmitter<MenuOpenState> = new EventEmitter()
+
+  public readonly directMenuChanged: EventEmitter<WheelSelectorItem[]> = new EventEmitter()
 
   constructor() {
     this.menuChanged.next({ items: [] })
