5c59e99a530d0697938c2454298f47e0f3618755
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-11-05 11:22:03 +0100

    Updated INIT SQL script

--- install-scripts/db/init.sql
+++ install-scripts/db/init.sql
@@ -1,19 +1,24 @@
-CREATE TABLE IF NOT EXISTS game(
-    game_id BIGINT PRIMARY KEY,
-    game_name VARCHAR(255) NOT NULL,
-    game_image_path VARCHAR(255),
-    game_path VARCHAR(255),
-    favorite BOOLEAN,
-    core_core_id BIGINT,
-    description_game_description_id BIGINT
+CREATE TABLE IF NOT EXISTS game_genres(
+    id BIGINT,
+    genres VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS cores(
-    core_id BIGINT PRIMARY KEY,
-    core_name VARCHAR(255) NOT NULL,
-    library_path VARCHAR(255),
-    priority INTEGER,
-    associated_platform_platform_id BIGINT
+
+CREATE TABLE IF NOT EXISTS platform_description(
+    platform_description_id BIGINT PRIMARY KEY,
+    description OID,
+    publisher VARCHAR(255),
+    release_date VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS platform(
+    platform_id BIGINT PRIMARY KEY,
+    display_name VARCHAR(255),
+    platform_image_path VARCHAR(255),
+    platform_name VARCHAR(255),
+    description_platform_description_id BIGINT,
+    CONSTRAINT FK_DESCRIPTION_PLATFORM_DESCRIPTION_ID
+        FOREIGN KEY (description_platform_description_id) REFERENCES platform_description(platform_description_id)
 );
 
 CREATE TABLE IF NOT EXISTS game_description(
@@ -24,31 +29,36 @@ CREATE TABLE IF NOT EXISTS game_description(
     publisher VARCHAR(255),
     rating FLOAT,
     release_date VARCHAR(255),
-    platform_platform_id BIGINT
+    platform_platform_id BIGINT,
+    CONSTRAINT FK_PLATFORM_PLATFORM_ID
+        FOREIGN KEY (platform_platform_id) REFERENCES  platform(platform_id)
 );
 
-CREATE TABLE IF NOT EXISTS game_genres(
-    id BIGINT,
-    genres VARCHAR(255)
+CREATE TABLE IF NOT EXISTS cores(
+    core_id BIGINT PRIMARY KEY,
+    core_name VARCHAR(255) NOT NULL,
+    library_path VARCHAR(255),
+    priority INTEGER,
+    associated_platform_platform_id BIGINT,
+    CONSTRAINT FK_ASSOCIATED_PLATFORM_PLATFORM_ID
+        FOREIGN KEY (associated_platform_platform_id) REFERENCES platform(platform_id)
 );
 
-CREATE TABLE IF NOT EXISTS platform(
- platform_id BIGINT PRIMARY KEY,
- display_name VARCHAR(255),
- platform_image_path VARCHAR(255),
- platform_name VARCHAR(255),
- description_platform_description_id BIGINT
-);
-
-CREATE TABLE IF NOT EXISTS platform_description(
-    platform_description_id BIGINT PRIMARY KEY,
-    description OID,
-    publisher VARCHAR(255),
-    release_date VARCHAR(255)
+CREATE TABLE IF NOT EXISTS game(
+    game_id BIGINT PRIMARY KEY,
+    game_name VARCHAR(255) NOT NULL,
+    game_image_path VARCHAR(255),
+    game_path VARCHAR(255),
+    favorite BOOLEAN,
+    core_core_id BIGINT,
+    description_game_description_id BIGINT,
+    CONSTRAINT FK_DESCRIPTION_GAME_DESCRIPTION_ID
+        FOREIGN KEY(description_game_description_id) REFERENCES game_description(game_description_id),
+    CONSTRAINT FK_CORE_CORE_ID
+        FOREIGN KEY(core_core_id) REFERENCES cores(core_id)
 );
 
 CREATE UNIQUE INDEX PK_GAME_ID ON game(game_id);
-CREATE INDEX FK_GAME_DESCRIPTION_ID ON game(description_game_description_id);
 CREATE UNIQUE INDEX PK_CORES_ID ON cores(core_id);
 CREATE UNIQUE INDEX PK_GAME_DESCRIPTION_ID ON game_description(game_description_id);
 CREATE UNIQUE INDEX PK_PLATFORM_ID ON platform(platform_id);
