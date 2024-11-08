135b988e7b0bae8f6c49b1f615390378c7a0843a
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-11-08 10:40:16 +0100

    Add apps menu unit tests

--- infoplay-ui/src/app/services/menu/apps/apps-menu.service.spec.ts
+++ infoplay-ui/src/app/services/menu/apps/apps-menu.service.spec.ts
@@ -1,16 +1,39 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 
+import { Subject } from 'rxjs';
+import { MenuOpenState } from '../../../models/core/state/menu-open-state';
+import { MenuStateService } from '../menu-state.service';
 import { AppsMenuService } from './apps-menu.service';
+import { applicationsMenu } from '../../../menus/applications';
 
 describe('AppsMenuService', () => {
   let service: AppsMenuService;
+  let menuStateService: MenuStateService
+
+  let menuOpenState: Subject<MenuOpenState>
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
+    menuStateService = TestBed.inject(MenuStateService)
+    menuOpenState = new Subject<MenuOpenState>()
+    spyOn(menuStateService, 'menuOpenedFiltered').and.returnValue(menuOpenState.asObservable())
     service = TestBed.inject(AppsMenuService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it("should call the menu generation when menu is asked to be opened", fakeAsync(() => {
+    const spy = spyOn(service, 'appsMenu')
+    menuOpenState.next({ menuId: AppsMenuService.ID })
+    tick(1)
+    expect(spy).toHaveBeenCalled()
+  }))
+
+  it("should request for menuChange when appMenu is called", () => {
+    const spy = spyOn(menuStateService.menuChanged, 'next')
+    service.appsMenu()
+    expect(spy).toHaveBeenCalledWith({ items: applicationsMenu })
+  })
 });
