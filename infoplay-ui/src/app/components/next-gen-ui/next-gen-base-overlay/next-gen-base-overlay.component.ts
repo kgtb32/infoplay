18744981d9ec0dbeb43a38f5f147d2a7203c208c
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-11-28 12:34:24 +0100

    Add next-gen-base-overlay component

---
+++ infoplay-ui/src/app/components/next-gen-ui/next-gen-base-overlay/next-gen-base-overlay.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-next-gen-base-overlay',
+  templateUrl: './next-gen-base-overlay.component.html',
+  styleUrl: './next-gen-base-overlay.component.scss'
+})
+export class NextGenBaseOverlayComponent {
+
+}
