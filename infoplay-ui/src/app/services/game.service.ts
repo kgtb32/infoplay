0fbf9dc4854b8dfbf05ca1096e530e100d3baecf
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-12-09 11:17:56 +0100

    Add toggle favorites api caller

--- infoplay-ui/src/app/services/game.service.ts
+++ infoplay-ui/src/app/services/game.service.ts
@@ -9,10 +9,12 @@ import { Platform } from '../models/api/platform';
 })
 export class GameService {
 
-  private static readonly FAVORITES_URL = "/api/game/favorites"
-  private static readonly GAME_BY_PLATFORM_URL = "/api/game/platform/"
-  private static readonly PLAY_URL = "/api/game/run"
-  private static readonly PLATFORMS_URL = "/api/platform"
+  private static readonly BASE_URL = "/api"
+  private static readonly GAME_BASE_URL = GameService.BASE_URL + "/game"
+  private static readonly FAVORITES_URL = GameService.GAME_BASE_URL + "/favorites"
+  private static readonly GAME_BY_PLATFORM_URL = GameService.GAME_BASE_URL + "/platform/"
+  private static readonly PLAY_URL = GameService.GAME_BASE_URL + "/run"
+  private static readonly PLATFORMS_URL = GameService.BASE_URL + "/platform"
 
   constructor(private readonly httpClient: HttpClient) { }
 
@@ -30,6 +32,10 @@ export class GameService {
         letter
       }
     })
+  }
+
+  toggleFavoritesGames(id: number): Observable<WheelSelectorItem[]> {
+    return this.httpClient.put<WheelSelectorItem[]>(`${GameService.GAME_BASE_URL}/${id}/toggleFavorites`, {})
   }
 
   playGame(id: number): Observable<boolean> {
